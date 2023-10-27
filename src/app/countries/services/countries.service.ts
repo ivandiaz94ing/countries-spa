c04c96a635abe5ea0aa4b56bb5baa1ee1b935e95
Ivan Diaz <ivandiaz94ing@gmail.com>
2023-10-27 16:11:39 -0500

    add persistencia con el localstorage

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -19,7 +19,20 @@ export class CountriesService {
   byRegion:    { region: '', countries: [] },
 }
 
-  constructor(private http: HttpClient) {   }
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+
+   }
+
+  private saveToLocalStorage(){
+    localStorage.setItem( 'cacheStore', JSON.stringify( this.cacheStore) );
+  }
+
+  private loadFromLocalStorage(){
+    if( !localStorage.getItem('cacheStore') ) return;
+
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')! );
+  }
 
 
   private getCountryRequest(url: string):Observable<Country[]>{
@@ -45,7 +58,8 @@ export class CountriesService {
     const url = `${this.apiUrl }/capital/${term}`;
     return this.getCountryRequest(url)
     .pipe(
-      tap( countries => this.cacheStore.byCapital = { term, countries } )
+      tap( countries => this.cacheStore.byCapital = { term, countries } ),
+      tap( () => this.saveToLocalStorage()),
 );
   }
 
@@ -53,7 +67,8 @@ export class CountriesService {
     const url = `${this.apiUrl }/name/${term}`;
     return this.getCountryRequest(url)
     .pipe(
-      tap( countries => this.cacheStore.byCountries = { term, countries } )
+      tap( countries => this.cacheStore.byCountries = { term, countries } ),
+      tap( () => this.saveToLocalStorage()),
 );
 
   }
@@ -62,7 +77,8 @@ export class CountriesService {
     const url = `${this.apiUrl }/region/${region}`;
     return this.getCountryRequest(url)
     .pipe(
-      tap( countries => this.cacheStore.byRegion = { region, countries } )
+      tap( countries => this.cacheStore.byRegion = { region, countries } ),
+      tap( () => this.saveToLocalStorage()),
 );
   }
 
